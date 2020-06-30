82511abdfd5446f5362610f5f70eaa517dc6f3ea
Anthony Mandala <anthonymmandala1@gmail.com>
2020-06-30 16:50:12 -0400

    backend

---
+++ backend/routes/api.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/fullStack', (req, res, next) => {
+
+});
+
+router.post('/fullStack', (req, res, next) => {
+
+});
+
+router.delete('/fullStack', (req, res, next) => {
+
+});
+
+module.exports = router;
